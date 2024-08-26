1c25b01dafb38cea642a3335dbea564d7aea80b6
manzalawie <169066122+manzalawie@users.noreply.github.com>
2024-08-26 18:46:35 +0300

    Add files via upload

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import notFound from '../../assets/notFound.jfif'
+import notFound from '../../assets/error.svg'
 
 
 export default function Error() {
   return (
-    <div>
+    <div className='flex justify-center pt-20'>
       <img src={notFound} alt="" />
     </div>
   )
